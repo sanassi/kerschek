eeb1d887b5f1586f901b27c69f374a17a1e0f5d2
Sanassi Cisse <sanassicisse@outlook.fr>
2022-02-17 23:14:52 +0100

    moved files to src folder
    Signed-off-by: Sanassi Cisse <sanassicisse@outlook.fr>

---
+++ src/image_processing/ArrayConversion.c
@@ -0,0 +1,34 @@
+#include "ArrayConversion.h"
+
+double *BW_BitmapToArray(SDL_Surface *img)
+{
+	double *arr = malloc(img -> h * img -> w * sizeof(double));
+
+	Uint32 pixel;
+	Uint8 r, g, b;
+
+	for (int i = 0; i < img -> h; i++)
+	{
+		for (int j = 0; j < img -> w; j++)
+		{
+			pixel = get_pixel(img, j, i);
+
+			SDL_GetRGB(pixel, img -> format, &r, &g, &b);
+
+			if (r == 0 && g == 0 & b == 0)
+			{
+				arr[i * img -> w + j] = 1;
+			}
+			else
+			{
+				arr[i * img -> w + j] = 0; 
+			}
+
+			printf("%i ",(int) arr[i * img -> w + j]);
+		}
+
+		printf("\n");
+	}
+
+	return arr;
+}
---
+++ src/neural_network/MatrixOperations.h
@@ -0,0 +1,8 @@
+#ifndef MATRIX_OPERATIONS_H
+#define MATRIX_OPERATIONS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
+
+#endif
