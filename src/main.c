2eb9e268aeadba8fb63ed2de9c11713fc58cae52
Sanassi CISSE <sanassi-mory.cisse@epita.fr>
2022-03-31 12:01:25 +0200

    todo : cleanup
    Signed-off-by: Sanassi CISSE <sanassi-mory.cisse@epita.fr>

--- src/main.c
+++ src/main.c
@@ -5,21 +5,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "image_processing/Sobel.h"
-#include "image_processing/Blur.h"
-#include "image_processing/PreProcessing.h"
-#include "image_processing/SdlHelperFunctions.h" 
-#include "image_processing/Rescaling.h"
-#include "image_processing/Floodfill.h"
-#include "image_processing/Drawing.h"
-#include "image_processing/Component.h"
-#include "image_processing/ArrayConversion.h"
-#include "image_processing/Hough.h"
-#include "image_processing/Vector.h"
-#include "image_processing/Morph.h"
-#include "image_processing/Video.h"
+#include "../image_processing/Sobel.h"
+#include "../image_processing/Blur.h"
+#include "../image_processing/PreProcessing.h"
+#include "../image_processing/SdlHelperFunctions.h" 
+#include "../image_processing/Rescaling.h"
+#include "../image_processing/Floodfill.h"
+#include "../image_processing/Drawing.h"
+#include "../image_processing/Component.h"
+#include "../image_processing/ArrayConversion.h"
+#include "../image_processing/Hough.h"
+#include "../image_processing/Vector.h"
+#include "../image_processing/Morph.h"
+#include "../image_processing/Video.h"
 
-#include "image_processing/PlateFromImage.h"
+#include "../image_processing/PlateFromImage.h"
 
 #define ANGLE 3
 
